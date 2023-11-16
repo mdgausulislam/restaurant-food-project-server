2086370d843cbba819de8306bb13f01bea0c35ba
unknown <gausulislam5@gmail.com>
2023-11-16 22:00:58 +0600

    send jwt token verification to server

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, send) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(400).send({ error: true, message: 'unauthorized access' })
+    }
+    //bearer token 
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    })
+    req.decoded = decoded;
+    next();
+}
 
 
 
@@ -77,7 +90,7 @@ async function run() {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ token })
-          })
+        })
 
 
         app.get('/menu', async (req, res) => {
@@ -92,10 +105,15 @@ async function run() {
         })
 
         //Cart Collection api
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
             }
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
